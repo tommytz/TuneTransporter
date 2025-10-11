4e62e9712f4257d60ddd9280cf6fd87791aa1c50
tommytz <ttardizuch@gmail.com>
2025-10-11 15:59:34 +1300

    reading first metadata header

--- read_flac_metadata.go
+++ read_flac_metadata.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"io"
 	"encoding/binary"
+	"errors"
+	"fmt"
+	"io"
+	"os"
 )
 
 const FlacMarker uint32 = 0x664C6143
@@ -15,7 +16,13 @@ func check(e error) {
 	}
 }
 
-func main () {
+type blockHeader struct {
+	isLast    bool
+	blockType byte
+	blockSize uint32
+}
+
+func main() {
 	filename := os.Args[1]
 
 	fmt.Println(filename)
@@ -31,9 +38,31 @@ func main () {
 	err = binary.Read(reader, binary.BigEndian, &marker)
 	check(err)
 
-	if marker == FlacMarker {
-		fmt.Println("This is a flac file!")
-	} else {
-		fmt.Println("This is not!")
+	if marker != FlacMarker {
+		fmt.Println("This is not a flac file! Exiting...")
+		panic(errors.New("Cannot read a non-flac file"))
 	}
+
+	fmt.Println("This is a flac file!")
+
+	firstBlock := readBlockHeader(reader)
+
+	fmt.Println(firstBlock.isLast)
+	fmt.Println(firstBlock.blockType)
+	fmt.Println(firstBlock.blockSize)
+}
+
+func readBlockHeader(reader io.Reader) *blockHeader {
+	raw := make([]byte, 4)
+
+	_, err := reader.Read(raw)
+	check(err)
+
+	header := blockHeader{
+		isLast:    (raw[0] >> 7) == 1,
+		blockType: raw[0] & 0x7f,
+		blockSize: uint32(raw[1])<<16 | uint32(raw[2])<<8 | uint32(raw[3]),
+	}
+
+	return &header
 }
